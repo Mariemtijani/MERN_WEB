fe0f2fffbf0522d6ebbdbb81f1eacb93ce6f115d
mariemtijani <mariemtijani60@gmail.com>
2024-05-12 19:18:27 +0100

    making middleware

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,40 @@
-require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
-const connectDB = require('./config/dbConnect')
-const mongoose = require('mongoose')
 
-connectDB()
+// const mongoose = require('mongoose')
 
-mongoose.connection.once('open' , () => {
-    console.log('Connected to mongoDb');
-    app.listen(PORT , () => {
-        console.log(`Server running on port ${PORT}`);
+console.log(__dirname)
+
+//custom middleware logger
+app.use(logger);
+
+//middleware for handling urlencoded form data 
+app.use(express.urlencoded({ extended: false }));
+
+//middleware for json
+app.use(express.json());
+
+//Cross origin resuorce sharing
+// app.use(cors());
+
+//servw static files
+app.use('/', express.static(path.join(__dirname,'public')));
+
+app.get('/', (req,res) => {
+    res.send('hello world !');
 })
-})
-
-mongoose.connection.on('error', err => {
-    console.log(err)
-})
 
 
+app.use(errorHandler);
+
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`)
+});
 
 
 
